4b0e9e370a8702af76c96f1e618e8da87f24470b
Shaked <shakedabdu50@gmail.com>
2024-01-20 16:45:10 +0200

    add post image library launcher added

--- app/src/main/java/com/example/mobile/Modules/AddPost/AddPostFragment.kt
+++ app/src/main/java/com/example/mobile/Modules/AddPost/AddPostFragment.kt
@@ -9,45 +9,55 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.Navigation
 import com.example.mobile.BlueFragmentArgs
 import com.example.mobile.Model.Model
 import com.example.mobile.Model.Post
 import com.example.mobile.R
+import com.example.mobile.databinding.FragmentAddPostBinding
+import com.example.mobile.databinding.FragmentPostsBinding
 
 
 class AddPostFragment : Fragment() {
     private var headerTextField: EditText? = null
     private var descriptionTextField: EditText? = null
+//    private var  ivUploadImage: ImageView? = null
     private var saveButton: Button? = null
     private var cancelButton: Button? = null
     private var email: String? = null
     private var imageUri: String? = null
+    private  var _binding: FragmentAddPostBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
+
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_add_post, container, false)
-//        email = arguments?.let { AddPostFragmentArgs.fromBundle(it).EMAIL }
-        email = arguments?.getString("EMAIL")
+        _binding = FragmentAddPostBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val ivUploadImage = binding.ivAddPostUploadImage
 
+        ivUploadImage.setOnClickListener(::uploadImage)
         setupUI(view)
         return view
     }
 
     private fun setupUI(view: View) {
-        headerTextField = view.findViewById(R.id.etHeader)
-        descriptionTextField = view.findViewById(R.id.etDescription)
-        saveButton = view.findViewById(R.id.btnSavePost)
-        cancelButton = view.findViewById(R.id.btnCancelPost)
+        headerTextField = binding.etHeader
+        descriptionTextField = binding.etDescription
+//        ivUploadImage = binding.ivAddPostUploadImage
+        saveButton = binding.btnSavePost
+        cancelButton = binding.btnCancelPost
+
+//        ivUploadImage?.setOnClickListener {
+//            uploadImage(it)
+//        }
 
         cancelButton?.setOnClickListener {
             Navigation.findNavController(it).popBackStack(R.id.postsFragment, false)
@@ -71,6 +81,15 @@ class AddPostFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         menu.clear()
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    private fun uploadImage(view: View) {
+        resultLauncher.launch("image/*")
+    }
+
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        imageUri = it.toString()
+        binding.ivAddPostUploadImage.setImageURI(it)
     }
 
 
